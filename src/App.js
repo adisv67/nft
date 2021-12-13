8e34384d2f8eb2ee6ebc18b9380a27093702d256
Adesh S <adesh.sonawane@morfsys.com>
2021-12-14 04:50:39 +0530

    punklist iterated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CollectionCard from './components/CollectionCard';
 import './App.css';
 import {useState, useEffect} from 'react'
 import axios from 'axios';
+import PunkList from './components/PunkList';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
     const getMyNfts = async () => {
       const openseaData = await axios.get('https://testnets-api.opensea.io/assets?owner=0xDbB366bD9F34C2CAAb836faDA387D3284Af62048&order_direction=asc&offset=0&limit=10')
       console.log(openseaData.data.assets)
+      setPunkListData(openseaData.data.assets)
     }
     
     return getMyNfts();
@@ -20,12 +22,7 @@ function App() {
   return (
    <div className='app'>
      <Header />
-      <CollectionCard 
-        id={0} 
-        name={'Bandana Punk'} 
-        traits={[{'value': 7}]} 
-        image={'https://nftlabs.mypinata.cloud/ipfs/bafybeigqkficum3anns36jid3dxvc4yfauyuvqjulbg43n23qxn3ce3tyu'}  
-     />
+     <PunkList punkListData={punkListData} />
    </div>
   );
 }
